8515f0348f99fba3047ffc721cf4247282eb7408
Fernando Gobetti <fernando.gobetti@hotmail.com.br>
2024-04-11 17:13:10 -0300

    Crud Plan and DetailsPlan finished

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Plan;
+use App\Observers\PlanObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Plan::observe(PlanObserver::class);
     }
 }
